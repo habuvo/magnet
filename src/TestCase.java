447ac663d8c0c07606abd8c1758d72b2bbed0aea
habuvo <ilya@canto.ru>
2017-07-29 21:19:09 +0300

    improve view of code

--- src/TestCase.java
+++ src/TestCase.java
@@ -83,21 +83,20 @@ public class TestCase {
         Statement stmt = conn.createStatement();
         PreparedStatement psmt = conn.prepareStatement("INSERT INTO test (field) VALUES (?)");
 
-// 2. drop table and insert N records
+        // 2. trunc table and insert N records
         stmt.execute("CREATE TABLE IF NOT EXISTS test (field INTEGER)");
         stmt.execute("TRUNCATE test");
-
 
         for (int i = 1; i <= testCase.getN(); i++) {
             psmt.setInt(1, i);
             psmt.addBatch();
-
         }
         psmt.executeBatch();
         conn.commit();
+        System.out.println("Insert table execution time: "
+                + (System.currentTimeMillis() - timestamp) / 1000 + " sec");
 
-// 3. build xml from TEST.FIELD
-
+        // 3. build xml from TEST.FIELD
         DocumentBuilderFactory icFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = icFactory.newDocumentBuilder();
         Document doc = dBuilder.newDocument();
@@ -124,32 +123,34 @@ public class TestCase {
         r.close();
         stmt.close();
         conn.close();
+        System.out.println("Build XML execution time: "
+                + (System.currentTimeMillis() - timestamp) / 1000 + " sec");
 
-// 4. convert elements to attributes
+        // 4. convert elements to attributes
         Source xsl = new StreamSource(new File("convert.xsl"));
         tr = TransformerFactory.newInstance().newTransformer(xsl);
         Source xmlInput = new StreamSource(new File("1.xml"));
         Result xmlOutput = new StreamResult(new File("2.xml"));
         tr.transform(xmlInput, xmlOutput);
+        System.out.println("Convert to attributes time: "
+                + (System.currentTimeMillis() - timestamp) / 1000 + " sec");
 
-// 5. parse 2.xml and output sum to con
-
+        // 5. parse 2.xml and output summ to con
         dBuilder.reset();
         doc = dBuilder.parse(new File("2.xml"));
         Node root = doc.getFirstChild();
         NodeList children = root.getChildNodes();
-        int summ = 0;
+        long summ = 0;
         for (int i = 0; i < children.getLength(); i++) {
             Node node = children.item(i);
             NamedNodeMap attributes = node.getAttributes();
             Node field = attributes.getNamedItem("field");
-            summ += Integer.parseInt(field.getNodeValue());
+            summ += (long) Integer.parseInt(field.getNodeValue());
         }
 
-        System.out.println("Summ is " +summ);
-
-
-        System.out.println("Total execution time: "+(System.currentTimeMillis()-timestamp)/1000+" sec");
+        System.out.println("Summ is " + summ);
+        System.out.println("Total execution time: "
+                + (System.currentTimeMillis() - timestamp) / 1000 + " sec");
     }
 }
 
